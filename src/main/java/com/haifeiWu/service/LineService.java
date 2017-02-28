3a44725b27b77a4818aa51b84debb388ae3f9d54
lijx <3034089596@qq.com>
2017-02-28 17:19:32 +0800

    释放一路资源

--- src/main/java/com/haifeiWu/service/LineService.java
+++ src/main/java/com/haifeiWu/service/LineService.java
@@ -1,5 +1,10 @@
 package com.haifeiWu.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.haifeiWu.dao.LineDao;
+import com.haifeiWu.entity.PHCSMP_Line;
 import com.haifeiWu.entity.PHCSMP_Suspect;
 
 /**
@@ -8,16 +13,19 @@ import com.haifeiWu.entity.PHCSMP_Suspect;
  * @author WXY
  *
  */
+
 public interface LineService {
+	
+	
 	
 	/**
 	 * 开启一路录像，判断是否饱和，并维护RecordVideo_State字段,置0或1
 	 */
-	public void startLine(String bandID,String cardReader_ID);
+	public void startLine();
 	/**
 	 * 释放一路录像
 	 */
-	public void closeLine(String bandID,String cardReader_ID);
+	public void closeLine();
 	
 	
 }
