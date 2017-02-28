be8636a7cd6dd46f76dff284617d810b667c3f42
litt <1240062768@qq.com>
2017-02-28 17:52:27 +0800

    嫌疑人查询接口优化

--- src/main/java/com/haifeiWu/dao/SuspectDao.java
+++ src/main/java/com/haifeiWu/dao/SuspectDao.java
@@ -67,13 +67,13 @@ public interface SuspectDao extends DaoSupport<PHCSMP_Suspect> {
 	 * @param bandId
 	 * @return
 	 */
-	PHCSMP_Suspect selectPersonInforByBandID(int bandId);
+	PHCSMP_Suspect selectPersonInforByBandID(int bandId,int suspectId);
 
 	/**
 	 * 获取数据库中的部分待查嫌疑人数据或者出区嫌疑人数据
 	 * 
 	 * @return
 	 */
-	List<PHCSMP_Suspect> getCheckingSuspect(int is_OutOf);
+	List<PHCSMP_Suspect> getCheckingSuspect(int Process_ID);
 
 }
